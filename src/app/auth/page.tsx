02974d47cd35f6eaecba4c205f4e89aea1537e80
Arshia Faraji <arshiafaraji22@gmail.com>
2025-01-18 16:52:24 +0330

    Feat: Add disable auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,10 @@
+import DisabledFeature from "@/components/disabled-feature";
 import AuthForm from "./auth-form";
 
 export default async function LoginPage() {
+  const disableAuth = process.env.DISABLE_AUTH === "true";
+  if (disableAuth) {
+    return <DisabledFeature />;
+  }
   return <AuthForm />;
 }
---
+++ src/components/disabled-feature.tsx
@@ -0,0 +1,11 @@
+export default function DisabledFeature() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-2xl font-semibold">Feature Unavailable</h1>
+      <p className="mt-2 text-base">
+        We&apos;re working hard to bring this feature to you. Please check back
+        soon!
+      </p>
+    </div>
+  );
+}
