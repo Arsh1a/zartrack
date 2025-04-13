7939501ade7f4ac153bdb3e58f64544390c3a758
Arshia Faraji <arshiafaraji22@gmail.com>
2025-04-13 15:06:15 +0330

    Chore: remove caching for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import DisplayData from "./display-data";
 import axios from "axios";
 import { LatestPrices } from "@/types";
 
-export const revalidate = 110;
-
 export default async function HomePage() {
   const data = await axios.get(
     `${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/latest`
